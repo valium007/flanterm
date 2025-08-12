94c38384aca68614553db9efc70324ef16bee617
valium007 <49368101+valium007@users.noreply.github.com>
2025-08-12 18:30:00 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     cc::Build::new()
         .file("flanterm/flanterm.c")
         .file("flanterm/backends/fb.c")
-        .std("c11")
+        .flag("/std:c11")
         .flag("/GS-")
         .flag("/link /NODEFAULTLIB")
         .compile("flanterm")
