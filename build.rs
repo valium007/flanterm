c64c61c21f632268b17688561ca5cd49bc5ac147
valium007 <49368101+valium007@users.noreply.github.com>
2025-08-12 18:21:42 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,10 +2,8 @@ fn main() {
     cc::Build::new()
         .file("flanterm/flanterm.c")
         .file("flanterm/backends/fb.c")
-        .flag("-std=c11")
-        .flag("-ffreestanding")
-        .flag("-fno-stack-protector")
-        .flag("-fno-stack-check")
-        .flag("-mno-red-zone")
+        .std("c11")
+        .flag("/GS-")
+        .flag("/link /NODEFAULTLIB")
         .compile("flanterm")
 }
