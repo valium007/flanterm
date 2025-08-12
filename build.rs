20317e31a106cddec4f8f468b0e3ba681438f86b
valium007 <49368101+valium007@users.noreply.github.com>
2025-08-12 19:06:42 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,12 @@
 fn main() {
     cc::Build::new()
+        .compiler(Path::new("C:\\Users\\Admin\\Downloads\\clang-18.1.8-windows-amd64-msvc17-msvcrt\\clang-18.1.8-windows-amd64-msvc17-msvcrt\\bin\\clang.exe"))
         .file("flanterm/flanterm.c")
         .file("flanterm/backends/fb.c")
-        .flag("/std:c11")
-        .flag("/GS-")
-        .flag("/link /NODEFAULTLIB")
+        .flag("-std=c11")
+        .flag("-ffreestanding")
+        .flag("-fno-stack-protector")
+        .flag("-fno-stack-check")
+        .flag("-mno-red-zone")
         .compile("flanterm")
 }
